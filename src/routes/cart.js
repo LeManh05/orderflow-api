f019272122c65f1f42ad17e85235d9af1843b45a
LeManh05 <leducmanh2995@gmail.com>
2026-09-22 15:47:53 +0700

    feat: add cart routes with authentication middleware

--- src/routes/cart.js
+++ src/routes/cart.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import authMiddleware from "../middlewares/auth.js";
+import { addCartItem, getCart, updateCartItem, deleteCartItem, clearCart } from "../controllers/cart.js";
 
 const router = Router()
 router.post('/items',authMiddleware,addCartItem)
